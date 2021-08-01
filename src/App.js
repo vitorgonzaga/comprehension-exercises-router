9dc03321fe1249330e969bbd084aa70eb3111ae4
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-08-01 16:22:00 -0300

    handle strict access component...

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StrictAccess from './components/StrictAccess';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -10,14 +11,18 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          {/* utilizar ":" (doi pontos) siginifica que a palavra seguinte sera um alias para uma variável a ser definida na url */}
-          <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" /> } />
+          <Switch>
+            {/* utilizar ":" (doi pontos) siginifica que a palavra seguinte sera um alias para uma variável a ser definida na url */}
+            <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" /> } />
+            <Route path="/about" component={About} />
+            <Route path="/strict-access" render={() => <StrictAccess user={({ username: 'joao', password: '1234' })} /> } />
+            <Route exact path="/" component={Home} />
+          </Switch>
           <nav className="nav-style">
             <div className="link"><Link to="/home">Home</Link></div>
             <div className="link"><Link to="/users">Users</Link></div>
             <div className="link"><Link to="/about">About</Link></div>
+            <div className="link"><Link to="/strict-access">Strict Access</Link></div>
           </nav>
         </div>
       </BrowserRouter>
