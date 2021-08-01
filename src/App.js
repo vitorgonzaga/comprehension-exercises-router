7cd7e685df9127ecb939cd02dca58094d4ba771b
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-08-01 14:59:58 -0300

    items 7 until 9 done

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        {/* utilizar ":" (doi pontos) siginifica que a palavra seguinte sera um alias para uma variável a ser definida na url */}
-        <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" /> } />
-        <nav className="nav-style">
-          <div><Link to="/home">Home</Link></div>
-          <div><Link to="/users">Users</Link></div>
-          <div><Link to="/about">About</Link></div>
-        </nav>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          {/* utilizar ":" (doi pontos) siginifica que a palavra seguinte sera um alias para uma variável a ser definida na url */}
+          <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" /> } />
+          <nav className="nav-style">
+            <div className="link"><Link to="/home">Home</Link></div>
+            <div className="link"><Link to="/users">Users</Link></div>
+            <div className="link"><Link to="/about">About</Link></div>
+          </nav>
+        </div>
       </BrowserRouter>
-    );
+    )
   }
 }
 
