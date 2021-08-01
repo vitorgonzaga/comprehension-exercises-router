a12432760842484f17992c98f300c23ba5cf2a8c
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-08-01 14:10:58 -0300

    Adding links to components with React Router Dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/home" component={Home} />
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/users" component={Users} />
       </BrowserRouter>
     );
   }
